089c94a5ef1998daf8b88f07fcab3d9bfe2d66f8
patriciadunphy <dunphy.pm@gmail.com>
2020-02-11 16:52:32 +0100

    More match code

--- src/main/java/org/program/match/Match.java
+++ src/main/java/org/program/match/Match.java
@@ -4,6 +4,7 @@ import org.program.fighter.Fighter;
 import org.program.fighter.TournamentFighters;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -24,13 +25,44 @@ public class Match {
 
      */
     //Structure for matches
+//    public void gameStart(TournamentFighters tour) {
+//        int listSize = tour.getListSize();
+//        while (listSize != 0) {
+//            int r = (int) (Math.random() * (tour.getListSize()));
+//            System.out.println(tour.getAFighter(r));
+//            tour.removeFromTournament(tour.getAFighter(r));
+//            listSize -= 1;
+//        }
+//    }
     public void gameStart(TournamentFighters tour) {
+        List<Fighter> semiFinal = new ArrayList<Fighter>();
         int listSize = tour.getListSize();
-        while (listSize != 0) {
-            int r = (int) (Math.random() * (tour.getListSize()));
-            System.out.println(tour.getAFighter(r));
-            tour.removeFromTournament(tour.getAFighter(r));
+        while (listSize >=2){
+            int playerOne = (int) (Math.random() * (tour.getListSize()));
+            System.out.println("Lägger till player1 i semi-lista: "+tour.getAFighter(playerOne));
+            semiFinal.add(tour.getAFighter(playerOne));
+            tour.removeFromTournament(tour.getAFighter(playerOne));
             listSize -= 1;
+
+            int playerTwo = (int) (Math.random() * (tour.getListSize()));
+            System.out.println(tour.getAFighter(playerTwo));
+            tour.removeFromTournament(tour.getAFighter(playerTwo));
+            listSize -= 1;
+            System.out.println("------------");
+        }
+        listSize = semiFinal.size();
+        while (listSize >=2){
+            int playerOne = (int) (Math.random() * (tour.getListSize()));
+            System.out.println("I semifinal: " + semiFinal.get(playerOne));
+            //semiFinal.add(tour.getAFighter(playerOne));
+            semiFinal.remove(semiFinal.get(playerOne));
+            listSize -= 1;
+
+            int playerTwo = (int) (Math.random() * (tour.getListSize()));
+            System.out.println("I semifinal: " + semiFinal.get(playerTwo));
+            semiFinal.remove(semiFinal.get(playerTwo));
+            listSize -= 1;
+            System.out.println("------------");
         }
     }
 }
